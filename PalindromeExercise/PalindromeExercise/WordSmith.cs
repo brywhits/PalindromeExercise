1fa32b857def3f8bd4718b2c4164678be23f460b
Bryant Whitsett <bryantwhitsett71@gmail.com>
2023-06-06 00:18:14 -0400

    what a brain freeze

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace PalindromeExercise
+{
+	public class WordSmith
+	{
+		public bool IsAPalindrome(string word)
+		{
+			var reversed = "";
+
+			for (int i = word.Length - 1; i >= 0; i--)
+			{
+				reversed += word[i];
+			}
+
+			// Compare the priginal woprd with the reversed...
+			// return whether they are the same or not
+
+			if (reversed == word)
+			{
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+
+		}
+	}
+	
+}
+
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,13 +1,25 @@
 using System;
+using PalindromeExercise;
 using Xunit;
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData ("nursesrun", true)]
+        [InlineData ("lemel", true )]
+        [InlineData ("fork", false)]
+        [InlineData("apple", false)]
+
+        public void MyTest(string word, bool expected)
         {
+            var test = new WordSmith();
+
+            bool actual = test.IsAPalindrome(word);
+
+            Assert.Equal(expected, actual);
 
         }
     }
